b45e4969b7b6b163b1ef87b939cfb5e7a06e8a87
Leah Oshry <owlinnyc@gmail.com>
2022-09-20 22:01:32 -0400

    initial calculator functions

---
+++ app.js
@@ -0,0 +1,29 @@
+// add function
+const addNumber = (number1, number2) => {
+    return (number1 + number2);
+};
+
+console.log(addNumber(2,1));
+
+// subtract function
+const subtractNumber = (number1, number2) => {
+    return (number1 - number2);
+};
+
+console.log(subtractNumber(8,2));
+
+// multiply function
+const multiplyNumber = (number1, number2) => {
+    return (number1 * number2);
+};
+
+console.log (multiplyNumber(7,8));
+
+// divide function
+const divideNumber = (number1, number2) => {
+    return (number1 / number2);
+};
+
+console.log(divideNumber(100, 10));
+
+
