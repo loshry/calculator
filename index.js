e3fcdeeb7af689ea3c62d9b9f3853ed0a1e77c0f
Leah Oshry <owlinnyc@gmail.com>
2022-09-27 16:01:59 -0400

    got the number buttons to work

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
 const numberButtons = document.querySelectorAll(".number__buttonnumber");
 
 const operandButtons = document.querySelectorAll(".operand__buttonoperand");
-
+const multiply = document.getElementsByClassName("operand__multiply")[0];
+const divide = document.getElementsByClassName("operand__divide")[0];
+const minus = document.getElementsByClassName("operand__minus")[0];
+const plus = document.getElementsByClassName("operand__plus")[0];
 const equalsbutton = document.querySelector(".operand__results");
 
 const clearResultsButton = document.querySelector(".top-buttons__clear");
@@ -22,16 +25,26 @@ clearResultsButton.addEventListener("click" , () => {
 }  );
 
 // select numbers
-let operand1 = "";
+// while and then if > as long as numbers being clicked ok but if click operand button store numbers
 numberButtons.forEach((button) => {
-    button.addEventListener("click", () => 
-    
-    { 
-   display.value.concat(button);
+    button.addEventListener("click", () => {
+        let number1 = display.value += button.innerHTML;
+
+        if (button.innerHTML == "&#43;" || button.innerHTML == "&#8722;" || button.innerHTML == "&#215;" || button.innerHTML == "&#247;"){
+            number1 = display.value;
+        };
+
+        
+   
     });
-    
-    console.log(display.value);
+  
 });
+
+// multiply.addEventListener("click", () => {
+
+    // display.value = "*";
+
+// });
     // const buttonFive = document.getElementById("five")
 
     // buttonFive.addEventListener("click" , () => {
